20ac7933090f878bac89e31ace011f18d933d5b7
garrettvernon115 <gvernon115@gmail.com>
2025-10-04 13:56:19 -0400

    User Model

---
+++ Resources/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Google.Cloud.Firestore;
+
+namespace Leux.Models
+{
+    [FirestoreData]
+    public class User
+    {
+        [FirestoreProperty("user_id")]
+        public string UserId { get; set; }
+
+        [FirestoreProperty("email")]
+        public string Email { get; set; }
+
+        [FirestoreProperty("password")]
+        public string Password { get; set; }
+
+        [FirestoreProperty("username")]
+        public string Username { get; set; }
+    }
+}
+
