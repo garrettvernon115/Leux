35b93f0c6229adad09849197685d721ca8b57560
Your Name <ouettanouri226@gmail.com>
2025-10-06 17:17:29 -0700

    LoginPage complte

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Leux;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+
+        await DisplayAlert("Login Attempt",
+            $"Email: {email}\nPassword: {password}", "OK");
+
+        
+    }
+
+    private async void OnSignUpClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Sign Up", "Navigate to Sign Up page (coming soon)", "OK");
+    }
+}
