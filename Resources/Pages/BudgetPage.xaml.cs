9d4c3673b4e9ac9d40709d8aa7fef37e29f234e4
Brady Stoker <bs998321@ohio.edu>
2025-10-20 13:06:30 -0400

    Created a button that allows the user to input a budget. Not yet showing result on screen.

--- Resources/Pages/BudgetPage.xaml.cs
+++ Resources/Pages/BudgetPage.xaml.cs
@@ -8,4 +8,25 @@ public partial class BudgetPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void OnAddBudgetClicked(object sender, EventArgs e)
+    {
+        PopupOverlay.IsVisible = true;
+    }
+
+    private void OnCancelClicked(object sender, EventArgs e)
+    {
+        PopupOverlay.IsVisible = false;
+    }
+
+    private void OnSaveBudgetClicked(object sender, EventArgs e)
+    {
+        string name = BudgetNameEntry.Text;
+        string amountText = BudgetAmountEntry.Text;
+        DateTime date = BudgetDatePicker.Date;
+
+        // TODO: Validate and save budget data
+
+        PopupOverlay.IsVisible = false;
+    }
 }
