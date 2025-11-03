acc705a91ab1095d1d8d93e893ec61b6535b857f
Brady-S <stokerbrady@gmail.com>
2025-11-02 22:00:56 -0500

    Added report service page. Doesn't do anything yet.

---
+++ Services/ReportService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+using Leux.Resources.Models;
+
+namespace Leux.Services
+{
+    public class ReportService : IReportService
+    {
+        private readonly FirestoreDb _firestoreDb;
+        public ReportService(FirestoreDb firestoreDb)
+        {
+            _firestoreDb = firestoreDb;
+        }
+
+        // public async Task<ReportData> GetReportDataAsync(string userId) { }
+    }
+}
